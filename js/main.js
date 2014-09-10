30585c2cbfd815e90b81dc420199a875f7e5361f
Matt Cook <matt@lookitscook.com>
2014-09-09 20:53:01 -0500

    content layout

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
 $(function() {
 
 // create a map in the "map" div, set the view to a given place and zoom
-var map = L.map('map-2d',{ zoomControl:false }).setView([43.0830517,-89.4064204], 12);
+/*var map = L.map('map-2d',{ zoomControl:false }).setView([43.0830517,-89.4064204], 12);
 
 var Stamen_Watercolor = L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>',
 	subdomains: 'abcd',
 	minZoom: 1,
 	maxZoom: 16
-}).addTo(map);
+}).addTo(map);*/
+
+$('#carousel').carouFredSel({
+        items: 3,
+        width: "102%",
+        responsive: true,
+        prev: {
+            button: '.carousel-container .prev'
+        },
+        next: {
+            button: '.carousel-container .next'
+        }
+    });
 
 });
