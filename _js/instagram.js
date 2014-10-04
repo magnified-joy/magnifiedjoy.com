f9be688a95b85909ded5f2e2d049ed25d39dffcd
Matt Cook <matt@lookitscook.com>
2014-10-04 16:01:18 -0500

    instagram is live, social links

---
+++ _js/instagram.js
@@ -0,0 +1,15 @@
+$(function() {
+
+    var USER_ID = 1274737756;
+    var ACCESS_TOKEN = "208248046.467ede5.a38042dafab64283b48b142694c13b7d";
+  
+    var f = new Instafeed({
+        get: 'user',
+        userId: USER_ID,
+        limit: 4,
+        resolution: 'low_resolution',
+        sortBy: 'most-recent',
+        accessToken: ACCESS_TOKEN
+    }).run();
+    
+});
