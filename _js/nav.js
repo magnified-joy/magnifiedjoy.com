340267d8a8fe17cacab2e3b486ab62bf3aa3be0e
Matt Cook <matt@lookitscook.com>
2014-10-07 13:03:37 -0500

    disable expanding header on homepage

--- _js/nav.js
+++ _js/nav.js
@@ -1,41 +1,43 @@
 $(function() {
-  
-  var pTop = parseInt($('body').css('padding-top'));
-  var startSize = 340;
-  var endSize = 200;
-  
-  var startPadding = 70;
-  var endPadding = 0;
-  
-  var marginStart = 70;
-  var marginEnd = 40;
-  
-  var $l = $('.navigation .left');
-  var $c = $('.navigation .center .logo');
-  var $r = $('.navigation .right');
-  var $a = $('.navigation .link');
 
-  $(window).scroll(function(){
-    var p = (pTop-$(window).scrollTop())/pTop;
-    if(p >= 0){
-        if(p > 1) p = 1;
-        var w = endSize+(startSize-endSize)*p;
-        var m = (marginStart-marginEnd)*p;
-        $c.css({
-            width: w+'px',
-            'margin-left':(-w/2)+'px'
-        });
-        $l.css({
-            'padding-right': (startPadding*p)+'px'
-        });
-        $r.css({
-            'padding-left': (startPadding*p)+'px'
-        });
-        $a.css({
-            'margin-top': (marginEnd+m)+'px'
-        });
-    }
+  if(!$('body').hasClass('home')){
+      var pTop = parseInt($('body').css('padding-top'));
+      var startSize = 340;
+      var endSize = 200;
+      
+      var startPadding = 70;
+      var endPadding = 0;
+      
+      var marginStart = 70;
+      var marginEnd = 40;
+      
+      var $l = $('.navigation .left');
+      var $c = $('.navigation .center .logo');
+      var $r = $('.navigation .right');
+      var $a = $('.navigation .link');
 
-  });
+      $(window).scroll(function(){
+        var p = (pTop-$(window).scrollTop())/pTop;
+        if(p >= 0){
+            if(p > 1) p = 1;
+            var w = endSize+(startSize-endSize)*p;
+            var m = (marginStart-marginEnd)*p;
+            $c.css({
+                width: w+'px',
+                'margin-left':(-w/2)+'px'
+            });
+            $l.css({
+                'padding-right': (startPadding*p)+'px'
+            });
+            $r.css({
+                'padding-left': (startPadding*p)+'px'
+            });
+            $a.css({
+                'margin-top': (marginEnd+m)+'px'
+            });
+        }
+
+      });
+  }
   
 });
