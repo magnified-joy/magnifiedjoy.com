87a6b6729d7696e120afc41384d4de8e2b790eb8
Matt Cook <matt@lookitscook.com>
2014-10-07 12:49:45 -0500

    removed scroll fx from portfolio page

--- _js/portfolio.js
+++ _js/portfolio.js
@@ -1,27 +1,12 @@
 $(function() {
 
-    var SCROLL_AMOUNT = 1;
-
     $(window).resize(resize);
 
     resize();
 
-    $('.navigation').css({position:'absolute',width:'100%',background:'#fff'});
-
-    var pTop = parseInt($('body').css('padding-top'));
-    
-    scroll();
-
-    function scroll(){
-      if(document.body.scrollTop < pTop){
-        window.scrollBy(0,SCROLL_AMOUNT);
-        setTimeout(scroll,1000/60);
-      }
-    }
-
     function resize(){
         $(".carousel, .carousel .item, .carousel-inner .item img").css({
-            height: window.innerHeight+10+"px"
+            height: window.innerHeight-parseInt($('body').css('padding-top'))+"px"
         });
     }
 
