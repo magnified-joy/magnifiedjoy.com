9b1185bb0fc3d1f083910f95f71cafc232a6619a
Matt Cook <matt@lookitscook.com>
2014-10-07 08:24:04 -0500

    adjusted logo scaling

--- _js/nav.js
+++ _js/nav.js
@@ -1,24 +1,28 @@
 $(function() {
   
   var pTop = parseInt($('body').css('padding-top'));
-  var startSize = 170;
-  var endSize = 100;
+  var startSize = 340;
+  var endSize = 200;
   
   var startPadding = 70;
   var endPadding = 0;
   
+  var marginStart = 70;
+  var marginEnd = 40;
+  
   var $l = $('.navigation .left');
   var $c = $('.navigation .center .logo');
-  var $r = $('.navigation .right')
+  var $r = $('.navigation .right');
+  var $a = $('.navigation .link');
   
   $(window).scroll(function(){
     var p = (pTop-document.body.scrollTop)/pTop;
     if(p >= 0){
-        var w = (startSize-endSize)*p;
+        var w = endSize+(startSize-endSize)*p;
+        var m = (marginStart-marginEnd)*p;
         $c.css({
-            width: (endSize+w)+'%',
-            'margin-left':(-w/2)+'%'
-            
+            width: w+'px',
+            'margin-left':(-w/2)+'px'
         });
         $l.css({
             'padding-right': (startPadding*p)+'px'
@@ -26,6 +30,10 @@ $(function() {
         $r.css({
             'padding-left': (startPadding*p)+'px'
         });
+        $a.css({
+            'margin-top': (marginEnd+m)+'px'
+        });
+        console.log(m);
     }
 
   });
