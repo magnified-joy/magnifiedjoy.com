5cb77af6dda26980c1070caf8ce02fe8a94ef570
Matt Cook <matt@lookitscook.com>
2014-10-06 19:54:32 -0500

    reworked portfolio to show image at original aspect ratio full screen

--- _js/portfolio.js
+++ _js/portfolio.js
@@ -1,14 +1,27 @@
 $(function() {
 
-$(window).resize(resize);
+    var SCROLL_AMOUNT = 1;
 
-resize();
+    $(window).resize(resize);
 
-function resize(){
-    var h = window.innerHeight-$('.navigation').height()-20;
-    $(".carousel, .carousel .item, .carousel-inner .item img").css({
-        height: h+"px"
-    });
-}
+    resize();
+
+    $('.navigation').css({position:'absolute',width:'100%',background:'#fff'});
+
+    scroll();
+
+    function scroll(){
+      console.log(document.body.scrollTop);
+      if(document.body.scrollTop < $('.navigation').height()+30){
+        window.scrollBy(0,SCROLL_AMOUNT);
+        setTimeout(scroll,1000/60);
+      }
+    }
+
+    function resize(){
+        $(".carousel, .carousel .item, .carousel-inner .item img").css({
+            height: window.innerHeight+10+"px"
+        });
+    }
 
 });
