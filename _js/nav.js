e02a86b45507554d4bc2c5db21d8dea39f827ac4
Matt Cook <matt@lookitscook.com>
2014-10-09 20:22:25 -0500

    iOS safari bug fix

--- _js/nav.js
+++ _js/nav.js
@@ -6,4 +6,11 @@ $(function() {
        });
   }
   
+  //ipad and iphone fix
+    if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.match(/iPad/i))) {
+        $("a.thumbnail").on('click touchstart mousedown touchend mouseup',function(){
+            window.location = $(this)el.attr('href');
+        });
+    }
+  
 });
