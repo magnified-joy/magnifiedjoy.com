7fd1d0d9478be6d1d64b25e838fde535c9619ff9
Matt Cook <matt@lookitscook.com>
2014-10-10 08:09:13 -0500

    fix for #18, finally!

--- _js/portfolio.js
+++ _js/portfolio.js
@@ -3,10 +3,15 @@ $(function() {
     $(window).resize(resize);
 
     resize();
-
+å
     function resize(){
+        var h = window.innerHeight-parseInt($('body').css('padding-top'))+'px';
+        var w = window.innerWidth+'px';
         $(".carousel, .carousel .item, .carousel-inner .item img").css({
-            height: window.innerHeight-parseInt($('body').css('padding-top'))+"px"
+            height: h
+        });
+        $('.carousel-inner .item img').attr('height',h).attr('width',w).css({
+            width: w
         });
     }
 
