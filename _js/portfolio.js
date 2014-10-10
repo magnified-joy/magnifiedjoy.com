07976a86c30ea00878fc4d13205d985be4610e01
Matt Cook <matt@lookitscook.com>
2014-10-10 08:23:19 -0500

    adjustments to portfolio appearance on mobile, related to #18

--- _js/portfolio.js
+++ _js/portfolio.js
@@ -1,17 +1,20 @@
 $(function() {
+  
+    var MIN_HEIGHT = 350;
 
     $(window).resize(resize);
 
     resize();
-å
+
     function resize(){
-        var h = window.innerHeight-parseInt($('body').css('padding-top'))+'px';
-        var w = window.innerWidth+'px';
+        var h = window.innerHeight-parseInt($('body').css('padding-top'));
+        var w = window.innerWidth;
+        if(h < MIN_HEIGHT) h = MIN_HEIGHT;
         $(".carousel, .carousel .item, .carousel-inner .item img").css({
-            height: h
+            height: h+'px'
         });
         $('.carousel-inner .item img').attr('height',h).attr('width',w).css({
-            width: w
+            width: w+'px'
         });
     }
 
