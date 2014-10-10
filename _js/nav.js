9c901c1fbcd26fa2ff7b6056c9561a36457b9caf
Matt Cook <matt@lookitscook.com>
2014-10-10 12:20:27 -0500

    fixes #33

--- _js/nav.js
+++ _js/nav.js
@@ -6,6 +6,15 @@ $(function() {
        });
   }
   
+  $('.carousel').carousel({
+        interval:2000,
+        pause: "false"
+    });
+  
+    $('.carousel-control').click(function(){
+        $(this).parents('.carousel').carousel('pause');
+    });
+  
     if(navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/iPad/i)){
         $("a.thumbnail").on('click touchstart mousedown touchend mouseup',function(){
             window.location = $(this).attr('href');
